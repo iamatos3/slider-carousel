777f47809a44b8796deb8d86993113b32c7c0d86
iamatos3 <matosian93@gmail.com>
2023-06-13 14:49:31 -0400

    Created a new component using React Slick
    - Updated the app to display the new component
    - Updated styling via CSS
    - Updated data

--- src/data.js
+++ src/data.js
@@ -18,16 +18,16 @@ export const list = [
     name: 'maria ferguson',
     title: 'office manager',
     quote:
-      'Fingerstache umami squid, kinfolk subway tile selvage tumblr man braid viral kombucha gentrify fanny pack raclette pok pok mustache.',
+      'Efficiency is the heart of productivity, and together we can create a workplace where success thrives.',
   },
   {
     id: 2,
     image:
       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
-    name: 'john doe',
+    name: 'john finnegan',
     title: 'customer success',
     quote:
-      'Gastropub sustainable tousled prism occupy. Viral XOXO roof party brunch actually, chambray listicle microdosing put a bird on it paleo subway tile squid umami.',
+      'Building strong relationships and exceeding customer expectations are the cornerstones of our mission to deliver exceptional success.',
   },
   {
     id: 3,
@@ -36,7 +36,7 @@ export const list = [
     name: 'peter smith',
     title: 'product designer',
     quote:
-      'Drinking vinegar polaroid street art echo park, actually semiotics next level butcher master cleanse hammock flexitarian ethical paleo.',
+      "Design has the power to shape experiences and ignite innovation, and as a product designer, I'm dedicated to crafting solutions that seamlessly blend functionality and beauty.",
   },
   {
     id: 4,
@@ -45,7 +45,7 @@ export const list = [
     name: 'susan andersen',
     title: 'CEO',
     quote:
-      'Marfa af yr 3 wolf moon kogi, readymade distillery asymmetrical seitan kale chips fingerstache cloud bread mustache twee messenger bag. ',
+      "I envision a future where bold visions become reality, where every challenge is an opportunity, and where our team's collective determination paves the way for extraordinary success.",
   },
 ];
 
